316a5342202a3213174b6890aa7ef1b8d6b0d371
dredodin <dredodin@gmail.com>
2025-02-15 13:52:23 +0400

    add Triangle

--- DesignPatterns.Visitor/DesignPatterns.Visitor/Models.cs
+++ DesignPatterns.Visitor/DesignPatterns.Visitor/Models.cs
@@ -4,4 +4,6 @@ public interface IFigure;
 
 public sealed record Circle(int R) : IFigure;
 
-public sealed record Rectangle(int A, int B):IFigure;
+public sealed record Rectangle(int A, int B): IFigure;
+
+public sealed record Triangle(int A, int B, int C): IFigure;
