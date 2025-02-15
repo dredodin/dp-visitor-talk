69424b1173688aeb9432024b5ca73a97bf665313
dredodin <dredodin@gmail.com>
2025-02-15 14:31:14 +0400

    Add `PublishAsyncVisitor` with state

---
+++ DesignPatterns.Visitor/DesignPatterns.Visitor/PublishUseCases.cs
@@ -0,0 +1,30 @@
+﻿namespace DesignPatterns.Visitor;
+
+internal sealed class PublishUseCases
+{
+    internal async Task PublishAsync(IEnumerable<IFigure> figures, CancellationToken ct)
+    {
+        foreach (var figure in figures)
+        {
+            await figure.Accept(new PublishAsyncVisitor(ct));
+        }
+    }
+
+    private sealed class PublishAsyncVisitor(CancellationToken ct) : IFigureVisitor<Task>
+    {
+        public async Task Visit(Circle circle)
+        {
+            await Task.Delay(1, ct);
+        }
+
+        public async Task Visit(Rectangle rectangle)
+        {
+            await Task.Delay(1, ct);
+        }
+
+        public async Task Visit(Triangle triangle)
+        {
+            await Task.Delay(1, ct);
+        }
+    }
+}
