45edde2ceb4568aa0f0a00e8df92a66b29cd62b0
dredodin <dredodin@gmail.com>
2025-02-19 13:01:09 +0400

    Add CarRentalBooking but forget of usecase

--- DesignPatterns.Visitor/DesignPatterns.Visitor/Models.cs
+++ DesignPatterns.Visitor/DesignPatterns.Visitor/Models.cs
@@ -5,3 +5,5 @@ public interface IBooking;
 public sealed record HotelBooking(string HotelName, DateTime CheckInDate, bool IncludesBreakfast) : IBooking;
 
 public sealed record FlightBooking(string FlightNumber, DateTime DepartureTime, string SeatClass) : IBooking;
+
+public sealed record CarRentalBooking(string RentalCompany, string CarModel, DateTime PickupDate, bool IncludesInsurance) : IBooking;
