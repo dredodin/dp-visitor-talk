0dbe99c0ba4d6ee56ba7fa8fef85ac8db48614da
Dmitrii Bessonov <Dmitrii_Bessonov@epam.com>
2025-03-30 18:29:58 +0400

    Add Homework project and implement game logic
    - Updated the solution file to include a new "Homework" project.
    - Added `IGameObject` interface and `Fruit` and `Monster` records in `GameObjects.cs`.
    - Introduced `Hero` record in `Hero.cs` for player movement and strength.
    - Created `Homework.csproj` targeting .NET 9.0 with necessary dependencies.
    - Implemented game logic in `Program.cs`, including object management and user input handling.
    - Added a `Draw` method for visualizing the game state in the console.
    - Updated `Readme.md` with game overview, rules, features, and homework tasks.

---
+++ DesignPatterns.Visitor/Homework/GameObjects.cs
@@ -0,0 +1,25 @@
+﻿namespace Homework;
+
+public interface IGameObject
+{
+    int X { get; set; }
+    int Y { get; set; }
+}
+
+public record Fruit : IGameObject
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+public record Monster(int Strength) : IGameObject
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+}
+
+//public record Obstacle : IGameObject
+//{
+//    public int X { get; set; }
+//    public int Y { get; set; }
+//}
